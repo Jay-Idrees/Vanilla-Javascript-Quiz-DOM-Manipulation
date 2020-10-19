f4eaa0d0685b4bdb9fd15ebe8d4aa9a004032142
Jay Idrees <idreesjay@gmail.com>
2020-10-18 21:08:27 -0400

    Finalized the main loading page

---
+++ script.js
@@ -0,0 +1,121 @@
+
+// DOM Elements
+//==========================================
+
+// User communication vectors 
+                 // I N P U T S
+// Welcome page: Start button
+// Radio buttons for each of the questions
+
+// OPTIONAL:
+// clicking mark
+// clicking to highlight text
+// clicking to underling text
+// clicking to cross out text
+// 
+
+
+
+
+
+
+                // O U T P U T S
+// Timer
+// Progress bar
+// Warning signs with time
+// Result
+// Heighlighting, marking, underlining, 
+
+// Other variables
+    // questions object
+    // Score
+
+
+// Features to be included later
+
+     // Progress bar to track question
+     // timer started on clicking start
+     // Next/Previous button
+     // Mark a question
+     // hightlight text of selected question
+     // cross-out text of selected question
+     // Underline text
+     // Pause exam for a break 
+     // Review marked/unanswered questions
+     // Storing score of correct and wrong answers
+     // Warning when one minute left
+     // Modal when time is up
+     // Displaying result in the end
+     // Close button
+     // Disabeling copying of test
+     // Disabeling mouse right click
+
+
+//====================================
+// F U N C T I O N S
+//====================================
+
+function showQ(){
+
+}
+
+function hideQ(){
+
+}
+
+function timerStart(){
+
+}
+
+function rightOrWrong(){
+
+}
+
+function linkProgBar(){
+
+}
+
+function saveAns(){
+
+}
+
+function displayScore() {
+    
+}
+
+
+
+//=====================================
+//  E V E N T   S E Q U E N C E
+//=====================================
+
+// Clicking start button should bring the first question
+
+// Clicking start should start the timer
+
+// CLicking the next button should make the current question disappear
+
+// Clicking the next button should save user response
+
+// determine if the question is correct or wrong
+
+// update the progress bar
+
+// Present the next question
+
+// Repeat steps for the subsequent questions
+
+// Warning modal at 1 minute
+
+// Warning modal when time up
+
+// Exam end screen with final score 
+
+
+//Optional
+// Any time
+      // User can click marked- save info
+      // User can hightlight/underline/cross text
+      // Review for unanswered and 
+      // pause the timer for a break of 1 minute
+      // If longer than 1 minute is passed the timer resumes automatically
