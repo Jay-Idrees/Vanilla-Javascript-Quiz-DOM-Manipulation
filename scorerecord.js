4f8a9bd938fc0bb35580fac58d6a4d5eab494339
Jay Idrees <idreesjay@gmail.com>
2020-12-12 13:39:56 -0500

    Adding functionality to give user an option to clear previous scores from local storage

--- scorerecord.js
+++ scorerecord.js
@@ -8,7 +8,7 @@ var highscoresList = JSON.parse(localStorage.getItem("code-quiz-highscores"));
 // console.log(highscoresList);
 
 if (highscoresList) {
-    // Sort the highscores from highest to lowest scores
+    // Sort the scores from highest to lowest scores
     highscoresList.sort(function(a, b){return b.score - a.score}); // from https://www.w3schools.com/js/js_array_sort.asp
     
     highscoresList.forEach(function(highscore, index) {
@@ -18,3 +18,11 @@ if (highscoresList) {
     });
 }
 
+// Giving an option to the user to clear previous scores in local storage
+clearButton.addEventListener("click", function(event) {
+    event.preventDefault();
+// Removing the scores from local storage
+    localStorage.removeItem("code-quiz-highscores");
+    // Clearing the hTML display from the high scores
+    highscoresListEl.innerHTML = "";
+})
